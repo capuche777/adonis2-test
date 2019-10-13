1deb39878da7022f0823936d039f061447f100aa
Jeremias Enriquez <capuche777@gmail.com>
2019-10-12 21:17:25 -0600

    Se renderizan paginas con layout e include

--- app/Controllers/Http/PageController.js
+++ app/Controllers/Http/PageController.js
@@ -2,11 +2,25 @@
 
 class PageController {
     home({ view }) {
-        return view.render('welcome')
+        return view.render('pages/welcome')
     }
 
     contact({ view }) {
-        return view.render('contact')
+        return view.render('pages/contact')
+    }
+
+    edge({ view }) {
+        let user = {
+            name: 'Jeremias',
+            age: 30,
+            location: 'Guastatoya, EP',
+            loggedIn: false
+        };
+        return view.render('pages/learning', {user})
+    }
+
+    admin({ view }) {
+        return view.render('pages/account')
     }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -1,21 +1,15 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
-Route.get('/', 'PageController.home');
-Route.get('/contact', 'PageController.contact');
-Route.get('/:category/:min/:max', 'ItemController.index');
+// Route.any('*', 'PageController.home');
+Route.get('/', 'PageController.home').as('home');
+Route.get('/edge', 'PageController.edge').as('edge');
+Route.get('/account', 'PageController.admin').as('admin');
+Route.get('/contact', 'PageController.contact').as('contact');
+// Route.get('/:category/:min/:max', 'ItemController.index');
+
+Route.group(() => {
+    Route.get('/dragon-ball-z', () => `<h1>Dragon Ball Z</h1>`);
+    Route.get('/call-of-duty', () => `<h1>Call Of Duty</h1>`);
+}).prefix('games/ps4');
